54e518406acc339d05ab67c7322501e7973b7cb4
crywolfe <gwolfe2007@gmail.com>
2014-06-06 13:29:40 -0400

    playing with frequency

--- js/microphone.js
+++ js/microphone.js
@@ -35,9 +35,9 @@ MicrophoneSample.prototype.getMicrophoneInput = function() {
 MicrophoneSample.prototype.onStream = function(stream) {
   var input = context.createMediaStreamSource(stream);
   var filter = context.createBiquadFilter();
-  filter.frequency.value = 10.0;
+  filter.frequency.value = 60.0;
   filter.type = filter.NOTCH;
-  filter.Q = 10.0;
+  filter.Q = 100.0;
 
   var analyser = context.createAnalyser();
 
