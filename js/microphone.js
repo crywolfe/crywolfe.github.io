5cf3a3ad00e375d592c871cb0f8416fe215cd79a
crywolfe <gwolfe2007@gmail.com>
2014-06-07 02:09:56 -0400

    playing with frequencies

--- js/microphone.js
+++ js/microphone.js
@@ -1,20 +1,8 @@
 /*
  * Copyright 2013 Boris Smus. All Rights Reserved.
-
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
- /* modified by Gerald Wolfe 2014 */
+ /* modified extensively by Gerald Wolfe 2014 */
 
 
 navigator.getUserMedia = (navigator.getUserMedia ||
@@ -70,11 +58,11 @@ MicrophoneSample.prototype.visualize = function() {
   for (var i = 0; i < times.length; i++) {
     var value = times[i];
     var percent = value / 256;
-    var newWidth = this.WIDTH * percent * 2.1;
+    var newWidth = this.WIDTH * percent * 2;
 
     var newOffset = this.WIDTH - newWidth;
 
-    var barHeight = this.HEIGHT*2.2/times.length;
+    var barHeight = this.HEIGHT*2.5/times.length;
 
     drawContext.fillStyle = 'blue';
 
