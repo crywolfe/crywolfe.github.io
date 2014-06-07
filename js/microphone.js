50697e4d8dcc2c01d37a63286c6088b2b49814f0
crywolfe <gwolfe2007@gmail.com>
2014-06-07 00:38:21 -0400

    playing with frequencies

--- js/microphone.js
+++ js/microphone.js
@@ -66,7 +66,7 @@ MicrophoneSample.prototype.visualize = function() {
   for (var i = 0; i < times.length; i++) {
     var value = times[i];
     var percent = value / 256;
-    var newWidth = this.WIDTH * percent;
+    var newWidth = this.WIDTH * percent * 1.3;
 
     var newOffset = this.WIDTH - newWidth;
 
