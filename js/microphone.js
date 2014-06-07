e62f6e3d5579bb3fe5243a7551bdd8296c5ecaac
crywolfe <gwolfe2007@gmail.com>
2014-06-07 07:21:24 -0400

    playing with frequencies

--- js/microphone.js
+++ js/microphone.js
@@ -66,10 +66,10 @@ MicrophoneSample.prototype.visualize = function() {
 
     drawContext.fillStyle = 'blue';
 
-    drawContext.fillRect(newOffset,i * barHeight, 30, 5);
+    drawContext.fillRect(newOffset - 50,i * barHeight, 30, 5);
 
     drawContext2.fillStyle = 'blue';
-    drawContext2.fillRect(newOffset + 740,i * barHeight, 30, 5);
+    drawContext2.fillRect(newOffset + 640,i * barHeight, 30, 5);
   }
   requestAnimFrame(this.visualize.bind(this));
 };
