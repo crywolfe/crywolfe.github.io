6d091736ed6173abd2a6381364da91eda3d85858
crywolfe <gwolfe2007@gmail.com>
2014-06-08 14:34:02 -0400

    more styling

--- js/microphone.js
+++ js/microphone.js
@@ -26,9 +26,9 @@ MicrophoneSample.prototype.getMicrophoneInput = function() {
 MicrophoneSample.prototype.onStream = function(stream) {
   var input = context.createMediaStreamSource(stream);
   var filter = context.createBiquadFilter();
-  filter.frequency.value = 10.0;
+  filter.frequency.value = 60.0;
   filter.type = filter.BANDPASS;
-  filter.Q = .1;
+  filter.Q = 10;
 
   var analyser = context.createAnalyser();
 
