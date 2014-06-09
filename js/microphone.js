075904d8a3f24affd5bcdf41059cded06d22632b
crywolfe <gwolfe2007@gmail.com>
2014-06-08 23:39:55 -0400

    microphone restyling

--- js/microphone.js
+++ js/microphone.js
@@ -70,9 +70,9 @@ MicrophoneSample.prototype.visualize = function() {
     // // drawContext.fillRect(newOffset - 255,i * barHeight, 18, 1);
     // drawContext.fillRect(i * barWidth, newOffset, 2, 1);
 
-    var height = this.HEIGHT * percent + 4;
+    var height = this.HEIGHT * percent;
     var offset = this.HEIGHT - height;
-    console.log(offset);
+    console.log(percent);
     var barWidth = this.WIDTH/times.length;
     drawContext.fillStyle = 'blue';
     drawContext.fillRect(i * barWidth, offset, 15, 10);
